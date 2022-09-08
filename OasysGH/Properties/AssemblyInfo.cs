dc544de004a672d5bf1284c9d1d8c7b7669c614d
Kristjan Nielsen <Kristjan.Nielsen@arup.com>
2022-09-08 09:25:50 +0200

    line ending

--- OasysGH/Properties/AssemblyInfo.cs
+++ OasysGH/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OasysGH")]
-[assembly: AssemblyDescription("OasysGH provides a common framework for .NET (RhinoCommon) plugin development for Rhino 6 and later")]
+[assembly: AssemblyDescription("OasysGH provides a common framework for .NET (RhinoCommon) plugin development for Rhino 6 and later.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Oasys")]
 [assembly: AssemblyProduct("OasysGH")]
