5671c7d3c2a4ff9031fb249c1bc696cc21e4f424
Tilman Reinhardt <tilman.reinhardt@arup.com>
2022-10-04 09:47:23 +0200

    push symbols

--- OasysGH/OasysPluginInfo.cs
+++ OasysGH/OasysPluginInfo.cs
@@ -22,7 +22,7 @@ namespace OasysGH
 
   internal sealed class PluginInfo
   {
-    internal const string Version = "0.3.3";
+    internal const string Version = "0.3.4";
 
     private static readonly Lazy<OasysPluginInfo> lazy =
         new Lazy<OasysPluginInfo>(() => new OasysPluginInfo(
