cbeb30d4e660e1fd8be696c12565046c8096e008
Kristjan Nielsen <Kristjan.Nielsen@arup.com>
2022-10-18 14:47:48 +0200

    bring dropdown component to front

--- OasysGH/ComponentAttributes/DropDownComponentAttributes.cs
+++ OasysGH/ComponentAttributes/DropDownComponentAttributes.cs
@@ -386,6 +386,11 @@ namespace OasysGH.UI
           RectangleF rec = BorderBound[i];
           if (rec.Contains(e.CanvasLocation))
           {
+            bool selected = comp.Attributes.Selected;
+            comp.Attributes.Selected = true;
+            comp.OnPingDocument().BringSelectionToTop();
+            comp.Attributes.Selected = selected;
+
             unfolded[i] = !unfolded[i];
             // close any other dropdowns that may be unfolded
             for (int j = 0; j < unfolded.Count; j++)
