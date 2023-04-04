44b85669d5ef707514f8a22a33878350eeb6ec1a
Tilman Reinhardt <tilman.reinhardt@arup.com>
2023-04-04 11:49:00 +0200

    checkbox component attributes

---
+++ OasysGH/ComponentAttributes/Helpers/DropDownArrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace OasysGH.UI.Helpers
+{
+  /// <summary>
+  /// Method to draw a dropdown arrow
+  /// 
+  /// Call this method when overriding Render method
+  /// </summary>
+  public class DropDownArrow
+  {
+    public static void DrawDropDownButton(Graphics graphics, PointF center, Color colour, int rectanglesize)
+    {
+      Pen pen = new Pen(new SolidBrush(colour))
+      {
+        Width = rectanglesize / 8
+      };
+
+      graphics.DrawLines(
+        pen, new PointF[]
+        {
+          new PointF(center.X - rectanglesize / 4, center.Y - rectanglesize / 8),
+          new PointF(center.X, center.Y + rectanglesize / 6),
+          new PointF(center.X + rectanglesize / 4, center.Y - rectanglesize / 8)
+        });
+    }
+  }
+}
