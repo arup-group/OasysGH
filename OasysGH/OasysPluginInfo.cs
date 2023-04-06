a41b95b4c4759b2d2b9a2e1ee2a0cce8abf5b33f
Tilman Reinhardt <tilman.reinhardt@arup.com>
2023-04-06 13:39:23 +0200

    v0.6.3

--- OasysGH/OasysPluginInfo.cs
+++ OasysGH/OasysPluginInfo.cs
@@ -7,7 +7,7 @@ namespace OasysGH {
 
     // this is the one place to set the version in VS:
     // also update the version manually in OasysGH.csproj
-    public const string Version = "0.6.2";
+    public const string Version = "0.6.3";
   }
 
   public class OasysPluginInfo {
