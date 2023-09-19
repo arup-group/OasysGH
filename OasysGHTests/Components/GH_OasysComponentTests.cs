675c9c3bfa45e8e87d60561ec0995d0cf9dfdc0f
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-09-19 13:16:50 +0200

    add remove component from document test

---
+++ OasysGHTests/Components/GH_OasysComponentTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Grasshopper.GUI;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Attributes;
+using Grasshopper.Kernel.Types;
+using OasysGH.Components;
+using OasysGH.Components.Tests;
+using OasysGH.UI;
+using OasysGHTests.TestHelpers;
+using Xunit;
+
+namespace OasysGHTests.Components {
+  [Collection("GrasshopperFixture collection")]
+  public class GH_OasysComponentTests {
+    [Fact]
+    public void AddRemoveComponentTest() {
+      var comp = new DropDownComponent();
+      comp.CreateAttributes();
+
+      var doc = new GH_Document();
+      doc.AddObject(comp, true);
+      Assert.Single(doc.Objects);
+
+      doc.Objects.Remove(comp);
+      Assert.Empty(doc.Objects);
+    }
+  }
+}
