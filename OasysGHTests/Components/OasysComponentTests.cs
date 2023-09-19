edf09029df905458fc304fe462531a2e498b955a
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-09-19 16:06:35 +0200

    add remove from doc test

---
+++ OasysGHTests/Components/OasysComponentTests.cs
@@ -0,0 +1,21 @@
+﻿using Grasshopper.Kernel;
+using OasysGH.Components.Tests;
+using Xunit;
+
+namespace OasysGHTests.Components {
+  [Collection("GrasshopperFixture collection")]
+  public class OasysComponentTests {
+    [Fact]
+    public void AddRemoveComponentTest() {
+      var comp = new DropDownComponent();
+      comp.CreateAttributes();
+
+      var doc = new GH_Document();
+      doc.AddObject(comp, true);
+      Assert.Single(doc.Objects);
+
+      doc.Objects.Remove(comp);
+      Assert.Empty(doc.Objects);
+    }
+  }
+}
