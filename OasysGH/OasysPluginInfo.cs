7655ac88ea0e9d19ef1f50f8588e3ba6f6c2370b
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-09-20 13:17:52 +0200

    update version to 1.0.0

--- OasysGH/OasysPluginInfo.cs
+++ OasysGH/OasysPluginInfo.cs
@@ -2,10 +2,10 @@
 
 namespace OasysGH {
   public static class OasysGHVersion {
-    public const bool IsBeta = true;
+    public const bool IsBeta = false;
     // this is the one place to set the version in VS:
     // also update the version manually in OasysGH.csproj
-    public const string Version = "0.9.0";
+    public const string Version = "1.0.0";
   }
 
   public class OasysPluginInfo {
