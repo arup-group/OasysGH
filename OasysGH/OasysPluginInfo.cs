b708a60216724670505f49bb0a35dad65d7f111e
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-10-12 15:55:05 +0200

    uprev version

--- OasysGH/OasysPluginInfo.cs
+++ OasysGH/OasysPluginInfo.cs
@@ -5,7 +5,7 @@ namespace OasysGH {
     public const bool IsBeta = false;
     // this is the one place to set the version in VS:
     // also update the version manually in OasysGH.csproj
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
   }
 
   public class OasysPluginInfo {
