90c5895af62366619177e9a3bd3f70a1ee7f7f26
DominikaLos <dominika.los@arup.com>
2025-12-18 10:36:01 +0100

    updated incorrect values

--- OasysGHTests/Helpers/SqlReaderTests.cs
+++ OasysGHTests/Helpers/SqlReaderTests.cs
@@ -13,16 +13,16 @@ namespace OasysGHTests.Helpers {
     [Theory]
     [InlineData("IPE100", new double[5] { 0.1, 0.055, 0.0041, 0.0057, 0.007 })]
     [InlineData("CHS457x12.5", new double[2] { 0.457, 0.0125 })]
-    [InlineData("RHS100x50x10", new double[3] { 0.1, 0.05, 0.1 })]
-    [InlineData("RHS100x50x10.0", new double[3] { 0.1, 0.05, 0.1 })]
+    [InlineData("RHS100x50x10", new double[3] { 0.1, 0.05, 0.01 })]
+    [InlineData("RHS100x50x10.0", new double[3] { 0.1, 0.05, 0.01 })]
     public void GetCatalogueProfileValuesTest(string profileString, double[] expectedValues) {
       List<double> values = SqlReader.Instance.GetCatalogueProfileValues(profileString, filePath);
 
       Assert.Equal(expectedValues.Length, values.Count);
 
-      for (int i = 0; i < expectedValues.Length; i++){
-        Assert.True(Math.Abs(expectedValues[i] - values[i]) < 1e-6, $"Expected: {expectedValues[i]}, actual: {values[i]}");
-      }
+      for (int i = 0; i < expectedValues.Length; i++)
+        Assert.True(Math.Abs(expectedValues[i] - values[i]) < 1e-6,
+          $"Expected: {expectedValues[i]}, actual: {values[i]}");
     }
 
     [Fact]
