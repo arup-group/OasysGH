f4b3c3ba3133d3ac5acc1ae8a16bb090b91b4f0e
spsarras <psarras.st@gmail.com>
2026-05-06 14:57:18 +0100

    reverse engineer strings to IProfile

--- OasysGH/Parameters/ProfileGoo.cs
+++ OasysGH/Parameters/ProfileGoo.cs
@@ -1,5 +1,6 @@
 ﻿using Grasshopper.Kernel.Types;
 using Oasys.Taxonomy.Profiles;
+using OasysGH.Helpers;
 
 namespace OasysGH.Parameters {
   public class OasysProfileGoo : GH_OasysGoo<IProfile> {
@@ -12,8 +13,31 @@ namespace OasysGH.Parameters {
 
     public OasysProfileGoo(IProfile item) : base(item) { }
 
+    public override bool CastFrom(object source) {
+      if (base.CastFrom(source))
+        return true;
+
+      string str = null;
+      if (source is string s)
+        str = s;
+      else if (source is GH_String gh)
+        str = gh.Value;
+
+      if (str != null) {
+        try {
+          Value = ProfileHelper.ProfileFromString(str);
+          return true;
+        } catch {
+          return false;
+        }
+      }
+
+      return false;
+    }
+
     public override IGH_Goo Duplicate() => new OasysProfileGoo(Value);
 
     public override string ToString() => Value.ToString();
   }
 }
+
