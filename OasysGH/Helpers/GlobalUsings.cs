002c9a31cd6dfec6f4663815cc1015d434067f85
Sandeep Kumar <sandeep.kumar@arup.com>
2026-08-19 10:13:40 +0530

    OGH-47:Bump version to 1.2.6

--- OasysGH/Helpers/GlobalUsings.cs
+++ OasysGH/Helpers/GlobalUsings.cs
@@ -1,9 +1,9 @@
-extern alias OasysGhSQLite_1_2_5;
+extern alias OasysGhSQLite_1_2_6;
 
 #if ISOLATED_SQLITE
-global using SQLiteConnection = OasysGhSQLite_1_2_5::System.Data.SQLite.SQLiteConnection;
-global using SQLiteCommand = OasysGhSQLite_1_2_5::System.Data.SQLite.SQLiteCommand;
-global using SQLiteDataReader = OasysGhSQLite_1_2_5::System.Data.SQLite.SQLiteDataReader;
+global using SQLiteConnection = OasysGhSQLite_1_2_6::System.Data.SQLite.SQLiteConnection;
+global using SQLiteCommand = OasysGhSQLite_1_2_6::System.Data.SQLite.SQLiteCommand;
+global using SQLiteDataReader = OasysGhSQLite_1_2_6::System.Data.SQLite.SQLiteDataReader;
 #else
 global using SQLiteConnection = System.Data.SQLite.SQLiteConnection;
 global using SQLiteCommand = System.Data.SQLite.SQLiteCommand;
